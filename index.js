d507cd47330579c20238e334c5aafd1ebb290fde
nou-ali <noura4403@gmail.com>
2021-12-03 20:07:10 +0000

    added functions

---
+++ index.js
@@ -0,0 +1,20 @@
+// const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+fetchCoordsByIP('174.91.166.119', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:' , coordinates);
+});
